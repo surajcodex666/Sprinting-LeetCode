e5db84f02250bc5f24a78fc54ae47a435d5c1e32
Suraj Yadav <surajyadavcodex666@gmail.com>
2026-07-22 10:21:20 +0530

    Time: 1 ms (93.04%), Space: 45.4 MB (41.29%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+
+        int ones = 0, twos = 0;
+
+        for(int i = 0; i < n; i++){
+            ones = (ones ^ nums[i]) & ~twos;
+            twos = (twos ^ nums[i]) & ~ones;
+        }
+        return ones;
+    }
+}
